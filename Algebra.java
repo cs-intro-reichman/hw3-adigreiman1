9aeef20e5882b592a8f84c1baeca7ad6e3166046
adigreiman1 <adigreiman@gmail.com>
2024-11-25 14:48:02 +0200

    Update Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -13,7 +13,7 @@ public class Algebra {
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		System.out.println(pow(5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
+   		System.out.println(div(-12,3));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
    		System.out.println(div(25,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
@@ -126,19 +126,39 @@ public class Algebra {
 		if (x2 == 0) {
 			return -1; 
 		}
-		else if (x1<x2){
+		else if (0<=x1 && x1<x2){
 		return 0;
 		}
+
+		boolean isNegative = false;
+    if (x1 < 0 && x2 < 0) {
+        x1 = minus(0, x1); 
+        x2 = minus(0, x2);
+
+    } else if (x1 < 0 || x2 < 0) {
+        isNegative = true; 
+		if (x1<0){
+			x1 = minus(0, x1); 
+		}
 		else{
-		//int count = 0; 
-		//while (x1 >= x2) { 
-		//	x1 = minus(x1, x2);
-		//	count++; 
-		//}
-	int div = times(x1, minus(0, x2));
-		return div; 
+			x2 = minus(0, x2);
+		}
 	}
-}
+       
+		int count = 0; 
+		while (x1 >= x2) { 
+		x1 = minus(x1, x2);
+			count++; 
+			
+			}
+		
+		if (isNegative == true) {
+			return minus(0, count);
+    }
+		return count; 
+	}
+
+
 	
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
@@ -152,14 +172,22 @@ public class Algebra {
 	}
 	return x1;
 }
+	
+
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		int guess = 0;
+		if (x <= 0) {
+			return -1; 
+
+		}
+		else{
+			int guess = 0;
 		while (times(guess, guess)<= x){
 			guess++;
 		}
         
 		return guess-1;
-	}	  	  
+	}	
+}  	  
 }
