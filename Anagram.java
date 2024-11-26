910d39e01d67c54a40689cbd8c785d202eb02ae8
adigreiman1 <adigreiman@gmail.com>
2024-11-26 16:10:10 +0200

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -4,7 +4,7 @@ public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
 		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
+		System.out.println(isAnagram("William Shakespeare","I am aweakish speller")); // true
 		System.out.println(isAnagram("Madam Curie","Radium came")); // true
 		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
 
@@ -29,16 +29,42 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+	String newStr1 = preProcess(str1);
+	String newStr2 = preProcess(str2);
+
+	if (newStr1.length() != newStr2.length()) {
+        return false;
+    }
+
+	for (int i =0; i<newStr1.length();i++){
+		boolean isCharExist = false;
+		for ( int j=0; j<newStr2.length();j++){
+			if(newStr1.charAt(i)==newStr2.charAt(j)){
+				isCharExist = true;
+				newStr2 = newStr2.substring(0, j) + newStr2.substring(j+1) ;
+			}
+		}
+		if (isCharExist ==false){
+			return false;
+		}
+	}
+	return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return str;
+		String newStr = "";
+		for (int i =0; i<str.length();i++){
+			if (str.charAt(i)>= 97 && str.charAt(i)<=122 && str.charAt(i)== 32){
+				newStr +=str.charAt(i);
+			}
+			else if (str.charAt(i)>=65 && str.charAt(i) <=90){
+				newStr += (char) (str.charAt(i)+32);
+			}
+		}
+		return newStr;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
